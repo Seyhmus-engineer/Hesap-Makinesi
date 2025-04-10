4f5a0cb7dc09808c249ae2009097d93d724b6a9c
Seyhmus Elik <elikseyhmus.21@gmail.com>
2025-04-10 22:11:48 +0300

    Add files via upload

---
+++ Hesap_Makinesi/Hesap_Makinesi/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace Hesap_Makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            no_9 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            no_0 = new Button();
+            button_C = new Button();
+            Mod = new Button();
+            button13 = new Button();
+            Bölme = new Button();
+            Carpma = new Button();
+            Cikarma = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            Esittir = new Button();
+            button10 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            textBox1.Location = new Point(21, 25);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(288, 95);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "0";
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Cursor = Cursors.Hand;
+            button1.Location = new Point(19, 208);
+            button1.Name = "button1";
+            button1.Size = new Size(45, 45);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Cursor = Cursors.Hand;
+            button2.Location = new Point(158, 272);
+            button2.Name = "button2";
+            button2.Size = new Size(45, 45);
+            button2.TabIndex = 2;
+            button2.Text = "6";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // no_9
+            // 
+            no_9.BackColor = SystemColors.ActiveCaption;
+            no_9.Cursor = Cursors.Hand;
+            no_9.Location = new Point(158, 334);
+            no_9.Name = "no_9";
+            no_9.Size = new Size(45, 45);
+            no_9.TabIndex = 4;
+            no_9.Text = "9";
+            no_9.UseVisualStyleBackColor = false;
+            no_9.Click += no_9_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ActiveCaption;
+            button5.Cursor = Cursors.Hand;
+            button5.Location = new Point(89, 334);
+            button5.Name = "button5";
+            button5.Size = new Size(45, 45);
+            button5.TabIndex = 5;
+            button5.Text = "8";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ActiveCaption;
+            button6.Cursor = Cursors.Hand;
+            button6.Location = new Point(19, 334);
+            button6.Name = "button6";
+            button6.Size = new Size(45, 45);
+            button6.TabIndex = 6;
+            button6.Text = "7";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ActiveCaption;
+            button7.Cursor = Cursors.Hand;
+            button7.Location = new Point(89, 272);
+            button7.Name = "button7";
+            button7.Size = new Size(45, 45);
+            button7.TabIndex = 7;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ActiveCaption;
+            button8.Cursor = Cursors.Hand;
+            button8.Location = new Point(19, 272);
+            button8.Name = "button8";
+            button8.Size = new Size(45, 45);
+            button8.TabIndex = 8;
+            button8.Text = "4";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.ActiveCaption;
+            button9.Cursor = Cursors.Hand;
+            button9.Location = new Point(158, 208);
+            button9.Name = "button9";
+            button9.Size = new Size(45, 45);
+            button9.TabIndex = 9;
+            button9.Text = "3";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // no_0
+            // 
+            no_0.BackColor = SystemColors.ActiveCaption;
+            no_0.Cursor = Cursors.Hand;
+            no_0.Location = new Point(89, 395);
+            no_0.Name = "no_0";
+            no_0.Size = new Size(45, 45);
+            no_0.TabIndex = 11;
+            no_0.Text = "0";
+            no_0.UseVisualStyleBackColor = false;
+            no_0.Click += no_0_Click;
+            // 
+            // button_C
+            // 
+            button_C.BackColor = SystemColors.ActiveCaption;
+            button_C.Cursor = Cursors.Hand;
+            button_C.Location = new Point(19, 144);
+            button_C.Name = "button_C";
+            button_C.Size = new Size(51, 45);
+            button_C.TabIndex = 12;
+            button_C.Text = "C";
+            button_C.UseVisualStyleBackColor = false;
+            button_C.Click += button_C_Click;
+            // 
+            // Mod
+            // 
+            Mod.BackColor = SystemColors.ActiveCaption;
+            Mod.Cursor = Cursors.Hand;
+            Mod.Location = new Point(89, 144);
+            Mod.Name = "Mod";
+            Mod.Size = new Size(51, 45);
+            Mod.TabIndex = 13;
+            Mod.Text = "Mod";
+            Mod.UseVisualStyleBackColor = false;
+            Mod.Click += Mod_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = SystemColors.ActiveCaption;
+            button13.Cursor = Cursors.Hand;
+            button13.Location = new Point(158, 144);
+            button13.Name = "button13";
+            button13.Size = new Size(51, 45);
+            button13.TabIndex = 14;
+            button13.Text = "Sil";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += button13_Click_1;
+            // 
+            // Bölme
+            // 
+            Bölme.BackColor = SystemColors.ActiveCaption;
+            Bölme.Cursor = Cursors.Hand;
+            Bölme.Location = new Point(221, 144);
+            Bölme.Name = "Bölme";
+            Bölme.Size = new Size(51, 45);
+            Bölme.TabIndex = 15;
+            Bölme.Text = "/";
+            Bölme.UseVisualStyleBackColor = false;
+            Bölme.Click += Bölme_Click;
+            // 
+            // Carpma
+            // 
+            Carpma.BackColor = SystemColors.ActiveCaption;
+            Carpma.Cursor = Cursors.Hand;
+            Carpma.Location = new Point(221, 208);
+            Carpma.Name = "Carpma";
+            Carpma.Size = new Size(51, 45);
+            Carpma.TabIndex = 16;
+            Carpma.Text = "x";
+            Carpma.UseVisualStyleBackColor = false;
+            Carpma.Click += Carpma_Click;
+            // 
+            // Cikarma
+            // 
+            Cikarma.BackColor = SystemColors.ActiveCaption;
+            Cikarma.Cursor = Cursors.Hand;
+            Cikarma.Location = new Point(221, 272);
+            Cikarma.Name = "Cikarma";
+            Cikarma.Size = new Size(51, 45);
+            Cikarma.TabIndex = 17;
+            Cikarma.Text = "-";
+            Cikarma.UseVisualStyleBackColor = false;
+            Cikarma.Click += Cikarma_Click;
+            // 
+            // button17
+            // 
+            button17.BackColor = SystemColors.ActiveCaption;
+            button17.Cursor = Cursors.Hand;
+            button17.Location = new Point(221, 334);
+            button17.Name = "button17";
+            button17.Size = new Size(51, 45);
+            button17.TabIndex = 18;
+            button17.Text = "+";
+            button17.UseVisualStyleBackColor = false;
+            button17.Click += button17_Click;
+            // 
+            // button18
+            // 
+            button18.BackColor = SystemColors.ActiveCaption;
+            button18.Cursor = Cursors.Hand;
+            button18.Location = new Point(21, 395);
+            button18.Name = "button18";
+            button18.Size = new Size(45, 45);
+            button18.TabIndex = 19;
+            button18.Text = ".";
+            button18.UseVisualStyleBackColor = false;
+            // 
+            // Esittir
+            // 
+            Esittir.BackColor = SystemColors.ActiveCaption;
+            Esittir.Cursor = Cursors.Hand;
+            Esittir.Location = new Point(158, 395);
+            Esittir.Name = "Esittir";
+            Esittir.Size = new Size(108, 45);
+            Esittir.TabIndex = 20;
+            Esittir.Text = "=";
+            Esittir.UseVisualStyleBackColor = false;
+            Esittir.Click += Esittir_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ActiveCaption;
+            button10.Cursor = Cursors.Hand;
+            button10.Location = new Point(89, 208);
+            button10.Name = "button10";
+            button10.Size = new Size(45, 45);
+            button10.TabIndex = 21;
+            button10.Text = "2";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click_1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.GradientActiveCaption;
+            label1.Location = new Point(35, 88);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 20);
+            label1.TabIndex = 22;
+            label1.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuHighlight;
+            ClientSize = new Size(332, 453);
+            Controls.Add(label1);
+            Controls.Add(button10);
+            Controls.Add(Esittir);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(Cikarma);
+            Controls.Add(Carpma);
+            Controls.Add(Bölme);
+            Controls.Add(button13);
+            Controls.Add(Mod);
+            Controls.Add(button_C);
+            Controls.Add(no_0);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(no_9);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Hesap Makinesi";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button no_9;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button no_0;
+        private Button button_C;
+        private Button Mod;
+        private Button button13;
+        private Button Bölme;
+        private Button Carpma;
+        private Button Cikarma;
+        private Button button17;
+        private Button button18;
+        private Button Esittir;
+        private Button numara2;
+        private Button button10;
+        private Label label1;
+    }
+}
